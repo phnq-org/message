c02bb17d8d769aa480bdb08db026f4016f8de5d6
Patrick Gostovic <patrick.gostovic@instacart.com>
2019-01-08 13:17:59 -0500

    Copy README.md to dist

--- scripts/postdist.js
+++ scripts/postdist.js
@@ -29,3 +29,5 @@ const distPkgJSON = {
 };
 
 fs.writeFileSync(path.resolve(__dirname, '../dist/package.json'), JSON.stringify(distPkgJSON, 0, 2));
+
+fs.copyFileSync(path.resolve(__dirname, '../README.md'), path.resolve(__dirname, '../dist/README.md'));

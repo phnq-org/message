a74c6797e831e0e85b99359dcea7c79f6681b582
Patrick Gostovic <patrick.gostovic@instacart.com>
2019-08-04 21:41:14 -0400

    Some lint errors

--- src/__tests__/NATSTransport.test.ts
+++ src/__tests__/NATSTransport.test.ts
@@ -1,61 +1,66 @@
 import { Client, connect } from 'ts-nats';
 import { Anomaly } from '../errors';
-import { MessageConnection } from '../MessageConnection';
+import { MessageConnection, Value } from '../MessageConnection';
 import { NATSTransport } from '../transports/NATSTransport';
 
-describe('NATSTransport', () => {
+describe('NATSTransport', (): void => {
   let nc: Client;
   let clientConnection: MessageConnection;
   let serverConnection: MessageConnection;
 
-  beforeAll(async () => {
-    nc = await connect();
-    clientConnection = new MessageConnection(
-      await NATSTransport.create(nc, { publishSubject: 's1', subscriptions: ['s2'] }),
-    );
-    serverConnection = new MessageConnection(
-      await NATSTransport.create(nc, { publishSubject: 's2', subscriptions: ['s1'] }),
-    );
-  });
+  beforeAll(
+    async (): Promise<void> => {
+      nc = await connect();
+      clientConnection = new MessageConnection(
+        await NATSTransport.create(nc, { publishSubject: 's1', subscriptions: ['s2'] })
+      );
+      serverConnection = new MessageConnection(
+        await NATSTransport.create(nc, { publishSubject: 's2', subscriptions: ['s1'] })
+      );
+    }
+  );
 
-  afterAll(() => {
+  afterAll((): void => {
     nc.close();
   });
 
-  describe('requests with multiple responses', () => {
-    it('should handle multiple responses with an async iterator', async () => {
-      serverConnection.onReceive<string>(message =>
-        (async function*() {
-          expect(message).toBe('knock knock');
+  describe('requests with multiple responses', (): void => {
+    it('should handle multiple responses with an async iterator', async (): Promise<void> => {
+      serverConnection.onReceive(
+        (message): AsyncIterableIterator<string> =>
+          (async function*(): AsyncIterableIterator<string> {
+            expect(message).toBe('knock knock');
 
-          yield "who's";
-          yield 'there';
-          yield '?';
-        })(),
+            yield "who's";
+            yield 'there';
+            yield '?';
+          })()
       );
 
       const resps1 = [];
-      for await (const resp of await clientConnection.request<string>('knock knock')) {
+      for await (const resp of await clientConnection.requestMulti('knock knock')) {
         resps1.push(resp);
       }
 
       expect(resps1).toEqual(["who's", 'there', '?']);
 
       const resps2 = [];
-      for await (const resp of await clientConnection.request<string>('knock knock')) {
+      for await (const resp of await clientConnection.requestMulti('knock knock')) {
         resps2.push(resp);
       }
 
       expect(resps2).toEqual(["who's", 'there', '?']);
     });
 
-    it('should handle a single returned response with an iterator', async () => {
-      serverConnection.onReceive<string>(async message => {
-        return `you said ${message}`;
-      });
+    it('should handle a single returned response with an iterator', async (): Promise<void> => {
+      serverConnection.onReceive(
+        async (message): Promise<string> => {
+          return `you said ${message}`;
+        }
+      );
 
       const resps1 = [];
-      for await (const resp of await clientConnection.requestMulti<string>('hello')) {
+      for await (const resp of await clientConnection.requestMulti('hello')) {
         resps1.push(resp);
       }
 
@@ -63,44 +68,53 @@ describe('NATSTransport', () => {
     });
   });
 
-  describe('requests with a single response', () => {
-    it('should handle a single returned response', async () => {
-      serverConnection.onReceive<string>(async message => {
-        return `you said ${message}`;
-      });
+  describe('requests with a single response', (): void => {
+    it('should handle a single returned response', async (): Promise<void> => {
+      serverConnection.onReceive(
+        async (message): Promise<string> => {
+          return `you said ${message}`;
+        }
+      );
 
-      const resp = await clientConnection.requestOne<string>('hello');
+      const resp = await clientConnection.requestOne('hello');
 
       expect(resp).toEqual('you said hello');
     });
 
-    it('should return the first response if multiple are provided', async () => {
-      serverConnection.onReceive<string>(message =>
-        (async function*() {
-          yield 'hey';
-          yield 'there';
-          yield message;
-        })(),
+    it('should return the first response if multiple are provided', async (): Promise<void> => {
+      serverConnection.onReceive(
+        (message): AsyncIterableIterator<Value> =>
+          (async function*(): AsyncIterableIterator<Value> {
+            yield 'hey';
+            yield 'there';
+            yield message;
+          })()
       );
 
-      const resp = await clientConnection.requestOne<string>('hello');
+      const resp = await clientConnection.requestOne('hello');
 
       expect(resp).toEqual('hey');
     });
   });
 
-  describe('one-way send (push)', () => {
-    it('should handle pushes in both directions', async () => {
+  describe('one-way send (push)', (): void => {
+    it('should handle pushes in both directions', async (): Promise<void> => {
       const serverReceive = jest.fn();
       const clientReceive = jest.fn();
 
-      serverConnection.onReceive<string>(async message => {
-        serverReceive(message);
-      });
+      serverConnection.onReceive(
+        async (message): Promise<undefined> => {
+          serverReceive(message);
+          return undefined;
+        }
+      );
 
-      clientConnection.onReceive<string>(async message => {
-        clientReceive(message);
-      });
+      clientConnection.onReceive(
+        async (message): Promise<undefined> => {
+          clientReceive(message);
+          return undefined;
+        }
+      );
 
       await Promise.all([clientConnection.send('one way'), serverConnection.send('or another')]);
 
@@ -109,14 +123,16 @@ describe('NATSTransport', () => {
     });
   });
 
-  describe('handling errors', () => {
-    it('should handle internal errors', async () => {
-      serverConnection.onReceive<string>(async message => {
-        throw new Error(`Error: ${message}`);
-      });
+  describe('handling errors', (): void => {
+    it('should handle internal errors', async (): Promise<void> => {
+      serverConnection.onReceive(
+        async (message): Promise<undefined> => {
+          throw new Error(`Error: ${message}`);
+        }
+      );
 
       try {
-        await clientConnection.requestOne<string>('hello');
+        await clientConnection.requestOne('hello');
         fail('Should have thrown');
       } catch (err) {
         expect(err).toBeInstanceOf(Error);
@@ -124,13 +140,15 @@ describe('NATSTransport', () => {
       }
     });
 
-    it('should handle anomalies', async () => {
-      serverConnection.onReceive<string>(async message => {
-        throw new Anomaly(`Anomaly: ${message}`, { foo: 'bar' });
-      });
+    it('should handle anomalies', async (): Promise<void> => {
+      serverConnection.onReceive(
+        async (message): Promise<undefined> => {
+          throw new Anomaly(`Anomaly: ${message}`, { foo: 'bar' });
+        }
+      );
 
       try {
-        await clientConnection.requestOne<string>('hello');
+        await clientConnection.requestOne('hello');
         fail('Should have thrown');
       } catch (err) {
         expect(err).toBeInstanceOf(Anomaly);
--- src/__tests__/serialize.test.ts
+++ src/__tests__/serialize.test.ts
@@ -1,11 +1,11 @@
 import { deserialize, serialize } from '../serialize';
 
-describe('serialize and deserialize', () => {
-  it('should serialize, deserialize back to the same thing', () => {
+describe('serialize and deserialize', (): void => {
+  it('should serialize, deserialize back to the same thing', (): void => {
     const obj = {
       date: new Date(),
       foo: 'bar',
-      nums: [1, 2, 3, 4, 5],
+      nums: [1, 2, 3, 4, 5]
     };
 
     expect(deserialize(serialize(obj))).toEqual(obj);

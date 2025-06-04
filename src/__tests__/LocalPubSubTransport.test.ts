da647a4c68b8e4602a41dc186a1d683de7eaf54f
Patrick Gostovic <Patrick.Gostovic@wmg.com>
2025-06-04 09:59:20 -0400

    Added LocalPubSubTransport

---
+++ src/__tests__/LocalPubSubTransport.test.ts
@@ -0,0 +1,203 @@
+import { Anomaly } from '../errors';
+import { MessageConnection } from '../MessageConnection';
+import { LocalPubSubTransport } from '../transports/LocalPubSubTransport';
+
+const wait = (millis = 0): Promise<void> =>
+  new Promise(resolve => {
+    setTimeout(resolve, millis);
+  });
+
+describe('LocalPubSubTransport', () => {
+  let clientConnection: MessageConnection<string | undefined, string | undefined>;
+  let serverConnection: MessageConnection<string | undefined, string | undefined>;
+  let clientObjectConnection: MessageConnection<object, object>;
+  let serverObjectConnection: MessageConnection<object, object>;
+
+  beforeAll(async (): Promise<void> => {
+    const signSalt = String(Date.now());
+
+    clientConnection = new MessageConnection<string | undefined, string | undefined>(
+      new LocalPubSubTransport({ publishSubject: 's1', subscriptions: ['s2'] }),
+      { signSalt },
+    );
+    serverConnection = new MessageConnection<string | undefined, string | undefined>(
+      new LocalPubSubTransport({ publishSubject: 's2', subscriptions: ['s1'] }),
+      { signSalt },
+    );
+
+    clientObjectConnection = new MessageConnection<object, object>(
+      new LocalPubSubTransport({ publishSubject: 'so1', subscriptions: ['so2'] }),
+      { signSalt },
+    );
+    serverObjectConnection = new MessageConnection<object, object>(
+      new LocalPubSubTransport({ publishSubject: 'so2', subscriptions: ['so1'] }),
+      { signSalt },
+    );
+  });
+
+  afterAll((): void => {
+    clientConnection.transport.close();
+    serverConnection.transport.close();
+  });
+
+  describe('requests with multiple responses', (): void => {
+    it('should handle multiple responses with an async iterator', async (): Promise<void> => {
+      serverConnection.onReceive = async (message): Promise<AsyncIterableIterator<string>> =>
+        (async function* (): AsyncIterableIterator<string> {
+          expect(message).toBe('knock knock');
+
+          yield "who's";
+          yield 'there';
+          yield '?';
+        })();
+
+      const resps1 = [];
+      for await (const resp of await clientConnection.requestMulti('knock knock')) {
+        resps1.push(resp);
+      }
+
+      expect(resps1).toEqual(["who's", 'there', '?']);
+
+      const resps2 = [];
+      for await (const resp of await clientConnection.requestMulti('knock knock')) {
+        resps2.push(resp);
+      }
+
+      expect(resps2).toEqual(["who's", 'there', '?']);
+    });
+
+    it('should handle a single returned response with an iterator', async (): Promise<void> => {
+      serverConnection.onReceive = async (message): Promise<string> => {
+        return `you said ${message}`;
+      };
+
+      const resps1 = [];
+      for await (const resp of await clientConnection.requestMulti('hello')) {
+        resps1.push(resp);
+      }
+
+      expect(resps1).toEqual(['you said hello']);
+    });
+  });
+
+  describe('requests with a single response', (): void => {
+    it('should handle a single returned response', async (): Promise<void> => {
+      serverConnection.onReceive = async (message): Promise<string> => {
+        return `you said ${message}`;
+      };
+
+      const resp = await clientConnection.requestOne('hello');
+
+      expect(resp).toEqual('you said hello');
+    });
+
+    it('should return the first response if multiple are provided', async (): Promise<void> => {
+      serverConnection.onReceive = async (message): Promise<AsyncIterableIterator<string | undefined>> =>
+        (async function* (): AsyncIterableIterator<string | undefined> {
+          yield 'hey';
+          yield 'there';
+          yield message;
+        })();
+
+      const resp = await clientConnection.requestOne('hello');
+
+      expect(resp).toEqual('hey');
+    });
+  });
+
+  describe('one-way send (push)', (): void => {
+    it('should handle pushes in both directions', async (): Promise<void> => {
+      const serverReceive = jest.fn();
+      const clientReceive = jest.fn();
+
+      serverConnection.onReceive = async (message): Promise<undefined> => {
+        serverReceive(message);
+        return undefined;
+      };
+
+      clientConnection.onReceive = async (message): Promise<undefined> => {
+        clientReceive(message);
+        return undefined;
+      };
+
+      await Promise.all([clientConnection.send('one way'), serverConnection.send('or another')]);
+
+      await wait(100);
+
+      expect(serverReceive).toHaveBeenCalledWith('one way');
+      expect(clientReceive).toHaveBeenCalledWith('or another');
+    });
+  });
+
+  describe('handling errors', (): void => {
+    it('should handle internal errors', async (): Promise<void> => {
+      serverConnection.onReceive = async (message): Promise<undefined> => {
+        throw new Error(`Error: ${message}`);
+      };
+
+      try {
+        await clientConnection.requestOne('hello');
+        fail('Should have thrown');
+      } catch (err) {
+        expect(err).toBeInstanceOf(Error);
+        expect((err as Error).message).toEqual('Error: hello');
+      }
+    });
+
+    it('should handle anomalies', async (): Promise<void> => {
+      serverConnection.onReceive = async (message): Promise<undefined> => {
+        throw new Anomaly(`Anomaly: ${message}`, { foo: 'bar' });
+      };
+
+      try {
+        await clientConnection.requestOne('hello');
+        fail('Should have thrown');
+      } catch (err) {
+        expect(err).toBeInstanceOf(Anomaly);
+        expect((err as Anomaly).message).toEqual('Anomaly: hello');
+        expect((err as Anomaly).info).toEqual({ foo: 'bar' });
+      }
+    });
+  });
+
+  describe('chunking', () => {
+    it('should handle messages that are larger than the max payload size', async (): Promise<void> => {
+      const SIZE = 5000000;
+      const buf = Buffer.alloc(SIZE, 'a');
+
+      let i = 0;
+      while (i < SIZE) {
+        buf[i] = 97 + Math.round(26 * Math.random());
+        i += Math.round(Math.random() * 1000);
+      }
+
+      const bigRandomString = buf.toString();
+
+      serverConnection.onReceive = async (): Promise<string> => {
+        return bigRandomString;
+      };
+
+      const resp = await clientConnection.requestOne('hello');
+
+      expect(resp).toEqual(bigRandomString);
+    });
+  });
+
+  describe('serialization/unserialization', () => {
+    it('should correctly pass a date object', async (): Promise<void> => {
+      const now = new Date();
+
+      serverObjectConnection.onReceive = async (message): Promise<object> => {
+        const { date } = message as { date: Date };
+        date.setSeconds(date.getSeconds() + 1);
+        return { date };
+      };
+
+      const resp = await clientObjectConnection.requestOne({ date: now });
+
+      const { date } = resp as { date: Date };
+
+      expect(date.getTime()).toEqual(now.getTime() + 1000);
+    });
+  });
+});
---
+++ src/transports/LocalPubSubTransport.ts
@@ -0,0 +1,96 @@
+import { MessageTransport, RequestMessage, ResponseMessage } from '../MessageTransport';
+import { annotate, deannotate } from '../serialize';
+
+type SubjectResolver<T, R> = (message: RequestMessage<T> | ResponseMessage<R>) => string;
+
+interface Options<T, R> {
+  subscriptions: string[];
+  publishSubject: string | SubjectResolver<T, R>;
+}
+
+export class LocalPubSubTransport<T, R> implements MessageTransport<T, R> {
+  private options: Options<T, R>;
+  private receiveHandler?: (message: RequestMessage<T> | ResponseMessage<R>) => void;
+  private subIds: number[] = [];
+
+  constructor(options: Options<T, R>) {
+    this.options = options;
+    this.subIds = options.subscriptions.map(sub =>
+      PubSub.instance.subscribe(sub, (message: RequestMessage<T> | ResponseMessage<R>) => {
+        if (this.receiveHandler) {
+          this.receiveHandler(message);
+        }
+      }),
+    );
+  }
+
+  async send(message: RequestMessage<T> | ResponseMessage<R>): Promise<void> {
+    const subject =
+      typeof this.options.publishSubject === 'function'
+        ? this.options.publishSubject(message)
+        : this.options.publishSubject;
+
+    const subCount = PubSub.instance.publish(subject, message);
+    if (subCount === 0) {
+      throw new Error(`No subscribers for subject: ${subject}`);
+    }
+
+    return undefined;
+  }
+  onReceive(receive: (message: RequestMessage<T> | ResponseMessage<R>) => void): void {
+    this.receiveHandler = receive;
+  }
+
+  async close(): Promise<void> {
+    for (const subId of this.subIds) {
+      PubSub.instance.unsubscribe(subId);
+    }
+    this.subIds = [];
+  }
+}
+
+class PubSub {
+  static instance = new PubSub();
+
+  private subIdIter = (function* subIdGen() {
+    let i = 0;
+    while (true) {
+      i += 1;
+      yield i;
+    }
+  })();
+  private subscriptions: Record<string, { subId: number; handler: (message: unknown) => void }[]> = {};
+
+  public publish(subject: string, message: RequestMessage<unknown> | ResponseMessage<unknown>): number {
+    const subs = this.subscriptions[subject] ?? [];
+    for (const sub of subs) {
+      /**
+       * This MessageTransport doesn't strictly require any marshalling or unmarshalling since the messages
+       * are sent synchronously within the same process. However, we still annotate and deannotate the message,
+       * effectively cloning it. The benefits are:
+       * - It ensures that the original message is not mutated by the subscriber.
+       * - It ensures compatibility with other transports.
+       */
+      const clonedMessage = deannotate(annotate(message));
+      sub.handler(clonedMessage);
+    }
+    return subs.length;
+  }
+
+  public subscribe<T, R>(subject: string, callback: (message: RequestMessage<T> | ResponseMessage<R>) => void): number {
+    const subs = this.subscriptions[subject] ?? [];
+    const subId = this.subIdIter.next().value;
+    subs.push({ subId, handler: callback as (message: unknown) => void });
+    this.subscriptions[subject] = subs;
+    return subId;
+  }
+
+  public unsubscribe(subId: number): void {
+    for (const subject in this.subscriptions) {
+      this.subscriptions[subject] = this.subscriptions[subject].filter(sub => sub.subId !== subId);
+      if (this.subscriptions[subject].length === 0) {
+        delete this.subscriptions[subject];
+      }
+    }
+  }
+}
